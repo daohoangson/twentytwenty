90db12ee55ed4039b0f80e4f5dbfcbf7f5d2f76b
Dao Hoang Son <daohoangson@gmail.com>
2017-09-04 02:56:19 +0700

    Add build script via Docker

---
+++ docker-run-internal.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+_minify="$( which minify )"
+if [ -z "$_minify" ]; then
+  yarn global add minifier
+fi
+
+if [ -d 'dist' ]; then
+  rm -rf dist
+fi
+mkdir dist
+
+minify -o ./dist/twentytwenty.min.css css/twentytwenty.css
+minify -o ./dist/twentytwenty.min.js js/jquery.event.move.js js/jquery.twentytwenty.js
